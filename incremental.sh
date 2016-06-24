481bb336aa08642a892e2709a1cc88801c2adc21
Nick Dumas <nickd@inmotionhosting.com>
2016-06-24 11:43:02 -0400

    Refactoring load testing bash script.

--- incremental.sh
+++ incremental.sh
@@ -1,10 +1,21 @@
-rate=1000
+function incremental_test(rate, duration, test_conf) {
+    #Setting the max rate should be configurable, but I don't want to have too many command line parameters. env var maybe?
+    while [ $rate -lt 1000000 ]; do 
+        echo -e "REQUEST RATE: $rate per second\n"
+        ./vegeta attack -targets $test_conf -duration $duration -rate $rate -output "$test_conf"."$rate".bin
+        ./vegeta report -inputs "$test_conf"."$rate".bin
+        #Report output probably shouldn't ASSUME you've got public_html/. Maybe dump the output to ./.reports/ or something? Use your best judgement here.
+        ./vegeta report -inputs "$test_conf"."$rate".bin -reporter plot > ~/public_html/reports/"$test_conf"."$rate".bin
+        echo -e "Report generated: '$test_conf'.'$rate'.html"
+        #The rate increment 'step' should be configurable as well.
+        let rate=$rate*10
+    done
+}
 
-while [ $rate -lt 1000000 ]; do
-    echo -e "REQUEST RATE: $rate per second\n"
-    ./vegeta attack -targets static_get.conf -duration 5s -rate $rate -output "$rate".bin
-    ./vegeta report -inputs "$rate".bin
-    ./vegeta report -inputs "$rate".bin -reporter plot > ~/public_html/reports/"$rate".html
-    echo -e "Report generated: '$rate'.html"
-    let rate=$rate*10
-done
+function main() {
+find ./tests/ -type f -exec incremental_test($1, $2, {}) \; 
+#This is the gist of what I want. Go over tests one at a time and run through the increments.
+#Also, help output would be nice, but not necessary.
+}
+
+main()
