27cc87d1337bc2a38cc4541efc494d662a503854
Nick Dumas <nickd@inmotionhosting.com>
2016-06-24 10:18:05 -0400

    Clearer terminal output.

--- incremental.sh
+++ incremental.sh
@@ -1,7 +1,7 @@
 rate=1000
 
 while [ $rate -lt 1000000 ]; do
-    echo -e "REQUEST RATE: $rate\n"
+    echo -e "REQUEST RATE: $rate per second\n"
     ./vegeta attack -targets static_get.conf -duration 5s -rate $rate -output "$rate".bin
     ./vegeta report -inputs "$rate".bin
     ./vegeta report -inputs "$rate".bin -reporter plot > ~/public_html/reports/"$rate".html
