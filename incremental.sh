df3976dd20a842de1a7c7f7a6b256248e029314a
Nick Dumas <nickd@inmotionhosting.com>
2016-06-24 08:56:26 -0400

    Adding more detail to the logging. Add stdout report.

--- incremental.sh
+++ incremental.sh
@@ -2,6 +2,9 @@ rate=1000
 
 while [ $rate -lt 1000000 ]; do
     echo -e "REQUEST RATE: $rate\n"
-    ./vegeta attack -targets static_get.conf -duration 5s -rate $rate| tee results.bin | ./vegeta report -reporter plot > ~/public_html/reports/"$rate".html
+    ./vegeta attack -targets static_get.conf -duration 5s -rate $rate -output "$rate".bin
+    ./vegeta report -inputs "$rate".bin
+    ./vegeta report -inputs "$rate".bin -reporter plot > ~/public_html/reports/"$rate".html
+    echo -e "Report generated: '$rate'.html"
     let rate=$rate*10
 done
