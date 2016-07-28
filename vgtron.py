9819c25ddc36b99e6de323b172fa12cc41ce515a
Nick Dumas <nickd@inmotionhosting.com>
2016-07-28 15:13:50 -0400

    Second pass over CLI flags.
    For this commit I'm changing things to use subcommands. This will allow
    for the exclusivity of the flags without making things weird.
    
    I'm still considering doing the exclusivity check at the application
    level rather than forcing argparse to do so.

--- vgtron.py
+++ vgtron.py
@@ -10,17 +10,21 @@ def vegeta(host):
 
 def main():
     parser = argparse.ArgumentParser(description='Vegeta convenience wrapper')
+    subparser = parser.add_subparsers()
+
+    target = subparser.add_parser('target')
+    target.add_argument('-f', '--file', dest='file', help='This is a file path to a Vegeta target file', nargs='?', default=None)
+
+    live = subparser.add_parser('live')
+    live.add_argument('-t', '--target', dest='target')
+    live.add_argument('-v', '--verb', dest='verb', default='GET', type=lambda v:v.upper())
 
     tuning_parser = parser.add_argument_group('Request Tuning', 'These flags are optional and can be provided to change the quantiy/duration of test requests.')
 
     tuning_parser.add_argument('-d', '--duration', dest='duration', default=10)
     tuning_parser.add_argument('-r', '--rate', dest='rate', default=1000)
     tuning_parser.add_argument('-s', '--step', dest='step', default=10)
-
-    parser.add_argument('-f', '--file', dest='file', nargs='?', default=None)
-
-    parser.add_argument('-t', '--target', dest='target')
-    parser.add_argument('-v', '--verb', dest='verb', default='GET', type=lambda v:v.upper())
+    tuning_parser.add_argument('-m', '--max', dest='max', default=10000)
 
     args = parser.parse_args()
 
